af510c2409a2b6842dae7ddc0a67cd73d2217673
Robert Spitzenpfeil <robert@linuxbox.site>
2011-03-07 18:04:02 +0100

    Got a working version of avr-gcc for the ATtiny4313 ;-)
    http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&t=104134

--- 2313/MBI5168_led_module_driver/flash.sh
+++ 2313/MBI5168_led_module_driver/flash.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-avrdude -c usbtiny -p attiny2313 -V -B 1 -e -U flash:w:$1:i
+#avrdude -c usbtiny -p attiny2313 -V -B 1 -e -U flash:w:$1:i
+
+# fuse settings for external crystal 8MHz and up
+# BOD: 2.7V
+avrdude -c usbtiny -p attiny4313 -U lfuse:w:0xff:m -U hfuse:w:0xdb:m -U efuse:w:0xff:m
+avrdude -c usbtiny -p attiny4313 -V -B 2 -e -U flash:w:$1:i
