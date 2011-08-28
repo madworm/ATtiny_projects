c76c9f362a2f76f1bb00871a3d0e9ece8d697260
Robert Spitzenpfeil <robert@linuxbox.site>
2011-08-28 14:29:49 +0200

    Moved some codes into #defines. Saves 60 bytes of flash.

--- 2313/Kitchen_Lights/led_driver.c
+++ 2313/Kitchen_Lights/led_driver.c
@@ -5,22 +5,29 @@
 #include "system_ticker.h"
 #include "status_leds.h"
 
+// timer1 setup
+// // set prescaler to 8 for a PWM frequency of about 1kHz (F_CPU = 8000000)
+// TCCR1B &= ~(_BV(CS12) | _BV(CS10));
+// TCCR1B |= _BV(CS11);
+// // set WGM mode 7: FAST PWM 10bit
+// TCCR1B &= ~_BV(WGM13);
+// TCCR1B |= _BV(WGM12);
+// TCCR1A |= (_BV(WGM11) | _BV(WGM10));
+// // connect OC1A (PB3) to the PWM generator (wired to the /ENABLE pin of the led driver)
+// TCCR1A |= _BV(COM1A1);
+// TCCR1A &= ~_BV(COM1A0);
+
+// default value of TCCR1A: 00000000
+#define TCCR1A_val ( _BV(WGM11) + _BV(WGM10) + _BV(COM1A1) )
+// default value of TCCR1B: 00000000
+#define TCCR1B_val ( _BV(CS11) + _BV(WGM12) )
+
 void led_driver_setup(void)
 {
-    // set compare match value for TCNT1
-    OCR1A = OCR1A_MAX; // 100% duty cycle - all off
-    // set prescaler to 8 for a PWM frequency of about 1kHz (F_CPU = 8000000)
-    TCCR1B &= ~(_BV(CS12) | _BV(CS10));
-    TCCR1B |= _BV(CS11);
-    // set WGM mode 7: FAST PWM 10bit
-    TCCR1B &= ~_BV(WGM13);
-    TCCR1B |= _BV(WGM12);
-    TCCR1A |= (_BV(WGM11) | _BV(WGM10));
-    // connect OC1A (PB3) to the PWM generator (wired to the /ENABLE pin of the led driver)
-    TCCR1A |= _BV(COM1A1);
-    TCCR1A &= ~_BV(COM1A0);
-    // make OC1A (PB3) an output
-    DDRB |= _BV(PB3);
+    TCCR1A = TCCR1A_val;    // doing it this way saves flash space ;-)
+    TCCR1B = TCCR1B_val;
+    DDRB |= _BV(PB3);       // make OC1A (PB3) an output
+    OCR1A = OCR1A_MAX;      // set compare match value for TCNT1 - 100% duty cycle - all off
 }
 
 void fade_in(uint16_t start_at, uint16_t fade_delay)
--- 2313/Kitchen_Lights/system_ticker.c
+++ 2313/Kitchen_Lights/system_ticker.c
@@ -4,21 +4,27 @@
 #include "system_ticker.h"
 #include "status_leds.h"
 
+// timer0 setup
+// setting prescaler to 8
+// TCCR0B |= _BV(CS01);
+// TCCR0B &= ~(_BV(CS00) | _BV(CS02));
+// // set WGM mode 0
+// TCCR0A &= ~(_BV(WGM01) | _BV(WGM00));
+// TCCR0B &= ~_BV(WGM02);
+// // normal operation - disconnect PWM pins
+// TCCR0A &= ~(_BV(COM0A1) | _BV(COM0A0) | _BV(COM0B1) | _BV(COM0B0));
+
+// default value of TCCR0A: 00000000
+#define TCCR0A_val ( 0 )
+// default value of TCCR0B: 00000000
+#define TCCR0B_val ( _BV(CS01) )
+
 volatile uint16_t system_ticks = 0;
 
 void system_ticker_setup(void)
 {
-    // using timer0
-    // setting prescaler to 8
-    TCCR0B |= _BV(CS01);
-    TCCR0B &= ~(_BV(CS00) | _BV(CS02));
-    // set WGM mode 0
-    TCCR0A &= ~(_BV(WGM01) | _BV(WGM00));
-    TCCR0B &= ~_BV(WGM02);
-    // normal operation - disconnect PWM pins
-    TCCR0A &= ~(_BV(COM0A1) | _BV(COM0A0) | _BV(COM0B1) | _BV(COM0B0));
-    // enabling overflow interrupt
-    TIMSK |= _BV(TOIE0);
+    TCCR0B = TCCR0B_val; // doing it this way saves flash space ;-)
+    TIMSK |= _BV(TOIE0); // enable overflow interrupt
 }
 
 ISR(TIMER0_OVF_vect) // on attiny2313/4313 this is named TIMER0_OVF_vect
