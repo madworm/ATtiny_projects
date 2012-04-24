439d9a09e3f4f12f1162aa89e4033d43b4b066de
Robert Spitzenpfeil <robert@linuxbox.site>
2012-04-24 11:01:34 +0200

    Fixed the bit-timings for avrdude (-B xxx).

--- 85/IR_raw_receiver/flash.sh
+++ 85/IR_raw_receiver/flash.sh
@@ -6,5 +6,5 @@ echo -e "\nTrying to flash: $HEXFILE\n"
 
 # ATtiny85 fuse settings for external crystal 8MHz
 # BOD: 2.7V
-avrdude -c usbtiny -p attiny85 -B 100 -U lfuse:w:0xDE:m -U hfuse:w:0xDD:m -U efuse:w:0xFF:m
-avrdude -c usbtiny -p attiny85 -V -B 1 -e -U flash:w:$HEXFILE:i
+avrdude -c usbtiny -p attiny85 -B 1000 -U lfuse:w:0xDE:m -U hfuse:w:0xDD:m -U efuse:w:0xFF:m
+avrdude -c usbtiny -p attiny85 -B 20 -e -U flash:w:$HEXFILE:i
