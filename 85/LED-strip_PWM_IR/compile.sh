0034038d5b1bf73bfdfea719c9f0942708f061ea
Robert Spitzenpfeil <robert@linuxbox.site>
2013-02-12 17:39:09 +0100

    ./85/LED-strip_PWM_IR: modified 'compile.sh' to use Arduino's avr-gcc.

--- 85/LED-strip_PWM_IR/compile.sh
+++ 85/LED-strip_PWM_IR/compile.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#
+# relative to your HOME directory
+#
+ARDUINO_DIR="arduino-1.0.3/hardware/tools/avr/bin"
+
+PATH=$PATH:$HOME/$ARDUINO_DIR
+
+if [[ ! -d ./obj/Debug ]]
+then
+mkdir -p "obj/Debug"
+fi
+
+if [[ ! -d ./bin/Debug  ]]
+then
+mkdir -p "bin/Debug"
+fi
+
 avr-g++ -mmcu=attiny85 -fexpensive-optimizations -Os -Wmain -Wall -g -gdwarf-2 -funsigned-bitfields -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -fno-exceptions -DF_CPU=8000000UL      -c IR_receiver.cpp -o obj/Debug/IR_receiver.o
 
 avr-g++ -mmcu=attiny85 -fexpensive-optimizations -Os -Wmain -Wall -g -gdwarf-2 -funsigned-bitfields -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -fno-exceptions -DF_CPU=8000000UL      -c main.cpp -o obj/Debug/main.o
@@ -8,7 +25,7 @@ avr-g++ -mmcu=attiny85 -fexpensive-optimizations -Os -Wmain -Wall -g -gdwarf-2 -
 
 avr-g++ -mmcu=attiny85 -fexpensive-optimizations -Os -Wmain -Wall -g -gdwarf-2 -funsigned-bitfields -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -fno-exceptions -DF_CPU=8000000UL      -c uart.cpp -o obj/Debug/uart.o
 
-avr-g++  -o bin/Debug/source.elf obj/Debug/IR_receiver.o obj/Debug/main.o obj/Debug/system_ticker.o obj/Debug/uart.o   -mmcu=attiny85 -Wl,-Map=bin/Debug/source.elf.map,--cref -Wl,--gc-sections,--print-gc-sections  /usr/local/avr/avr/lib/libc.a /usr/local/avr/avr/lib/libm.a 
+avr-g++  -o bin/Debug/source.elf obj/Debug/IR_receiver.o obj/Debug/main.o obj/Debug/system_ticker.o obj/Debug/uart.o   -mmcu=attiny85 -Wl,-Map=bin/Debug/source.elf.map,--cref -Wl,--gc-sections,--print-gc-sections -lm -lc -v
 
 avr-objcopy -O ihex -R .eeprom -R .eesafe bin/Debug/source.elf bin/Debug/source.elf.hex
 avr-objcopy --no-change-warnings -j .eeprom --change-section-lma .eeprom=0 -O ihex bin/Debug/source.elf bin/Debug/source.elf.eep.hex
