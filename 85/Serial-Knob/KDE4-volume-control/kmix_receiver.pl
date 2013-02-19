a1033f0b372677f93c1fb62097c668842633a650
Robert Spitzenpfeil <robert@linuxbox.site>
2013-02-19 20:18:28 +0100

    Updated 'kmix_receiver.pl': simultaneous keyboard + serial input.

--- 85/Serial-Knob/KDE4-volume-control/kmix_receiver.pl
+++ 85/Serial-Knob/KDE4-volume-control/kmix_receiver.pl
@@ -1,10 +1,15 @@
 #!/usr/bin/perl -w
 use strict;
 use Device::SerialPort;
+use Time::HiRes qw(usleep);
+
+use Term::ReadKey;
+ReadMode 4;
 
 my $port = "/dev/ttyUSB0"; # <-- find right port !
 my $link = Device::SerialPort->new($port) || die("could not open port: $port - $!"); # <-- match to right com port
-my $read = 0;
+my $readS = "";
+my $readC = "";
 
 $link->databits(8);
 $link->baudrate(9600); # <-- match to arduino settings
@@ -12,20 +17,31 @@ $link->parity("none");
 $link->stopbits(1);
 $| = 1; # buffers disabled
 
+print "\n\n Press 'q' to exit\n\n";
+
 while (1) {
-  $read = $link->read(1);
-    
-  if( $read eq "+" ) {
+  $readS = $link->read(1);
+  $readC = ReadKey(-1) || "";
+
+  if( ($readS eq "+") || ($readC eq "+") ) {
       # amarok 2 + KDE4: volume up
-      system('qdbus org.kde.kmix /kmix/KMixWindow/actions/increase_volume com.trolltech.Qt.QAction.trigger');
-    }
-  if( $read eq "-" ) {
-      # amarok 2 + KDE4: volume down
-      system('qdbus org.kde.kmix /kmix/KMixWindow/actions/decrease_volume com.trolltech.Qt.QAction.trigger');
+	  print "+";
+      system('qdbus org.kde.kmix /kmix/KMixWindow/actions/increase_volume com.trolltech.Qt.QAction.trigger > /dev/null');
   }
-  if( $read eq "/" ) {
+  if( ($readS eq "-") || ($readC eq "-") ) {
       # amarok 2 + KDE4: volume down
-      system('qdbus org.kde.kmix /kmix/KMixWindow/actions/mute com.trolltech.Qt.QAction.trigger');
+	  print "-";
+      system('qdbus org.kde.kmix /kmix/KMixWindow/actions/decrease_volume com.trolltech.Qt.QAction.trigger > /dev/null');
+  }
+  if( ($readS eq "/") || ($readC eq "/")  ) {
+      # amarok 2 + KDE4: volume down
+	  print "M";
+      system('qdbus org.kde.kmix /kmix/KMixWindow/actions/mute com.trolltech.Qt.QAction.trigger > /dev/null');
+  }
+
+  if( $readC eq "q" ) {
+    ReadMode 0;
+	exit;
   }
 
   # abuse select as a finer-granulated sleep function - limit cpu usage
@@ -33,6 +49,7 @@ while (1) {
   #
   # http://stackoverflow.com/questions/896904/how-do-i-sleep-for-a-millisecond-in-perl
   #
-  select(undef, undef, undef, 0.01);
+  # select(undef, undef, undef, 0.01);
+  usleep(5000);
 }
 
