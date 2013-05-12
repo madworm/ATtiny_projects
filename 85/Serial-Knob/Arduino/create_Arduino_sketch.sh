52afbad711962d1e391694850737cd3e15fb37ba
Robert Spitzenpfeil <robert@linuxbox.site>
2013-05-12 23:12:28 +0200

    Added script + patch files, so the Arduino-IDE can be used directly.

---
+++ 85/Serial-Knob/Arduino/create_Arduino_sketch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+. ../config.txt
+
+FOLDER=Serial_Knob
+ARDUINO_H=$HOME/$ARDUINO_DIR/hardware/arduino/cores/arduino/Arduino.h
+
+echo -e "\n\n  You will need this: http://hlt.media.mit.edu/?p=1695 / https://github.com/damellis/attiny/\n"
+
+if [[ ! -d ./$FOLDER ]]
+then
+	mkdir $FOLDER
+fi
+
+cp ../*.cpp ./$FOLDER
+cp ../*.hpp ./$FOLDER
+rename hpp h ./$FOLDER/*.hpp
+mv ./$FOLDER/main.cpp ./$FOLDER/Serial_Knob.ino
+
+for PATCH in `ls *.patch`
+do
+	patch ./$FOLDER/${PATCH/%.patch/} $PATCH
+done
+
+echo -e "\n  About to patch '$ARDUINO_H'\n"
+echo -e "  This should not have any negative effects. Just in case, there is a backup!\n"
+
+if [[ -e $ARDUINO_H.orig ]]
+then
+	echo -e "    Patch was already applied.\n"
+else
+	patch -b $ARDUINO_H Arduino.h__Serial-Knob.diff
+	echo -e "\n    Done!\n"
+fi
+
+if [[ -e $HOME/$ARDUINO_DIR/libraries/Ethernet ]]
+then
+	echo -e "  Disabling the 'Ethernet' library - moved to '$HOME/$ARDUINO_DIR'\n"
+	mv $HOME/$ARDUINO_DIR/libraries/Ethernet $HOME/$ARDUINO_DIR/	
+fi
+
+echo -e "  Move the folder '$FOLDER' to your Arduino 'sketchbook' folder.\n"
+
