fcb3c0909aa77339594e9ff6aaba9a6af95d58cb
Robert Spitzenpfeil <robert@linuxbox.site>
2013-05-13 17:47:16 +0200

    Tweaked the 'Serial_Knob' Arduino-IDE thing a bit. Updated README.TXT

--- 85/Serial-Knob/system_ticker.hpp
+++ 85/Serial-Knob/system_ticker.hpp
@@ -7,11 +7,21 @@
 #define ENC_COUNTS              7
 #define ENC_VELOCITY			8
 
-void system_ticker_setup(void);
-uint32_t millis(void);
-uint32_t micros(void);
-void delay(uint32_t ms);
-void delayMicroseconds(uint16_t us);
-int16_t encoder_get(uint8_t whatbit);
+// Added to make this Arduino-IDE compatible
+// This also requires to add '__attribute__ ((weak))' to a couple of functions
+// in 'Arduino.h': millis(), micros(), delay...()
+// See the .patch files in the 'Arduino' subfolder
+#ifdef __cplusplus
+extern "C" {
+#endif
+	void system_ticker_setup(void);
+	uint32_t millis(void);
+	uint32_t micros(void);
+	void delay(uint32_t ms);
+	void delayMicroseconds(uint16_t us);
+	int16_t encoder_get(uint8_t whatbit);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
