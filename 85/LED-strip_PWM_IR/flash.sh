de32146534a99bcda0b68dc7695578d1d341ff2c
Robert Spitzenpfeil <robert@linuxbox.site>
2013-05-17 20:49:03 +0200

    ./85/LED-strip_PWM_IR: tweaks.

--- 85/LED-strip_PWM_IR/flash.sh
+++ 85/LED-strip_PWM_IR/flash.sh
@@ -14,5 +14,11 @@ echo -e "Press <ENTER> to proceed\n"
 
 read
 
+# set FUSE settings
 avrdude -c avrispmkii -P usb -p attiny85 -B 1000 -U lfuse:w:0xDE:m -U hfuse:w:0xDD:m -U efuse:w:0xFF:m
+
+# write firmware
 avrdude -c avrispmkii -P usb -p attiny85 -B 1 -e -U flash:w:$HEXFILE:i
+
+# lock the chip
+# avrdude -c avrispmkii -P usb -p attiny85 -B 1000 -U lock:w:0xFC:m
